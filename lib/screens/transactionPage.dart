2d6c82304225a844900ec8bf7bc93814b9b3637e
Nabilah Daud <48626338+naveylah@users.noreply.github.com>
2021-07-01 00:38:16 +0800

    Update transactionPage.dart

--- lib/screens/transactionPage.dart
+++ lib/screens/transactionPage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:web3dart/web3dart.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:http/http.dart';
 import 'package:mad_mad/Models/sidebar.dart';
 import 'package:mad_mad/screens/homePage.dart';
-
-import '../colour.dart'; //at the top
+import '../colour.dart'; 
 
 class trxConfirm extends StatefulWidget {
   trxConfirm({Key key, this.title}) : super(key: key);
@@ -15,6 +16,80 @@ class trxConfirm extends StatefulWidget {
 }
 
 class _trxConfirm extends State<trxConfirm> {
+  String rpcUrl = "http://127.0.0.1:7545";
+  String wsUrl = "ws://127.0.0.1:7545/";
+  Future<void> sendEther() async {
+    Web3Client client = Web3Client(rpcUrl, Client(), socketConnector: () {
+      return IOWebSocketChannel.connect(wsUrl).cast<String>();
+    });
+
+    String privateKey =
+        "85be074b7d980d5547c1777ff46baa45953ab24755baa64d2cf5201f7d655fce";
+    Credentials credentials =
+        await client.credentialsFromPrivateKey(privateKey);
+    EthereumAddress receiver =
+        EthereumAddress.fromHex("0x61F7ed85BDd045a178327c9b740e4Fa52402eB5E");
+
+    EthereumAddress ownAddress = await credentials.extractAddress();
+    print(ownAddress);
+
+    client.sendTransaction(
+        credentials,
+        Transaction(
+            from: ownAddress,
+            to: receiver,
+            value: EtherAmount.fromUnitAndValue(EtherUnit.ether, 20)));
+  }
+
+  void popUp() {
+    sendEther();
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Stack(
+              clipBehavior: Clip.none,
+              children: <Widget>[
+                Positioned(
+                  right: -40.0,
+                  top: -40.0,
+                  child: InkResponse(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(
+                        child: Text('Transaction successful. Thank you.'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        // ignore: deprecated_member_use
+                        child: FlatButton(
+                          child: Text("ok"),
+                          color: darkBlue,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,45 +173,81 @@ class _trxConfirm extends State<trxConfirm> {
               color: Colors.white,
               child: SingleChildScrollView(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    SizedBox(
+                      height: 30,
+                    ),
                     Text(
-                      "English Literature",
+                      "English Literature Class",
                       style: TextStyle(
                           fontWeight: FontWeight.w700,
-                          fontSize: 17,
+                          fontSize: 20,
                           fontFamily: 'product'),
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
-                      "More than 30'000 PT3 Past Year Questions Exercise with Latest KSSR Syllabus. Get in touch with the tutors to learn and discuss your questions and answers",
+                      "Hello there! You are about to subscribe to English Literature Class for 18 ETH that will be deducted from your ethereum account!",
                       style: TextStyle(
                         fontFamily: 'circe',
-                        fontSize: 12,
+                        fontSize: 14,
                       ),
                     ),
                     SizedBox(
-                      height: 15,
+                      height: 30,
                     ),
+                    Container(
+                        width: 500,
+                        height: 151,
+                        color: yellow,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                Text(
+                                  "Transaction detail: ",
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline),
+                                ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  "\nClass: English Literature\nSubscription: 8 classes.\nTotal (ETH): 18",
+                                  style: TextStyle(
+                                      fontFamily: 'circe',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 30,
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        SizedBox(
+                          height: 60,
+                        ),
                         ElevatedButton(
                           style: ButtonStyle(
                               backgroundColor:
                                   MaterialStateProperty.all<Color>(darkBlue)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => trxConfirm()),
-                            );
-                          },
+                          onPressed: () => popUp(),
                           child: Text(
-                            'Start trial with 10 ether',
-                            style: GoogleFonts.lato(),
+                            'Proceed',
+                            style: TextStyle(fontSize: 15),
                           ),
                         )
                       ],
